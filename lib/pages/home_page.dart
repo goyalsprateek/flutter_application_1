e6f4e154232fdee8dbcdbd6685d15d927c55e351
Prateek Goel <goyalsprateek@gmail.com>
2021-12-14 20:06:29 +0530

    ppp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("My App 1"),
+        title: Text("My App 1 000"),
       ),
       body: Center(
         child: Padding(
